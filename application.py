4a0415070a71c224d1199b0c14da3684177c28b2
trice <trice@cisco.com>
2017-10-13 11:27:42 -0400

    converted to a microservice doing something useful

--- application.py
+++ application.py
@@ -1,33 +1,71 @@
 from flask import Flask
+from flask import jsonify
+from flask import request
+
+from pprint import pprint
+filename = 'ncaa.dat'
 
 # print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
+def say_hello():
+    return '<p>Hello NCAA</p>\n'
+
+# print a nice greeting.
+def lookup(param, val):
+    rows = []
+    for k in database.keys():
+        print k
+        if val in database[k][param]:
+            print '------ hit'
+            rows.append(database[k])
+    if not rows:
+        rows = {'error': 'no %s matches found for "%s"' % (param, val)}
+    return jsonify(rows)
 
 # some bits of text for the page.
 header_text = '''
     <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
 instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
+    <p>This is a RESTful micro service returning information on NCAA Div I football teams</p>
+    <UL>
+        <LI><I>param</I>/<i>search string</i><br/>where <I>param</I> is school, conference, mascot, city or state and  
+    </UL>\n''' 
+
 home_link = '<p><a href="/">Back</a></p>\n'
 footer_text = '</body>\n</html>'
 
+def teamdb():
+    db = {}
+    with open(filename) as f:
+        for line in f.read().splitlines():
+            cols = line.split("\t")
+            db[cols[0]] = {'school': cols[0],
+                           'mascot': cols[1],
+                           'city': cols[2],
+                           'state': cols[3],
+                           'conference': cols[4]
+                          }
+    print pprint(db.keys())
+    return db
+
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
+database = teamdb()
 
 # add a rule for the index page.
 application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
+    say_hello() + instructions + "example: %sschool/bama" % request.url_root + footer_text))
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+# # add a rule when the page is accessed with a name appended to the site URL.
+# application.add_url_rule('/<username>', 'hello', (lambda username:
+#     header_text + say_hello(username) + home_link + footer_text))
+
+# add a rule when the page is accessed with a name appended to the site URL.
+application.add_url_rule('/<param>/<school>', 'lookup', (lambda param, school:
+    lookup(param, school)))
 
 # run the app.
 if __name__ == "__main__":
+
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     application.debug = True
